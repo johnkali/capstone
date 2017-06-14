cc7ccd0bf976aa3ab0e3b1d06207f4b9ee715d34
johnkali <jdkali96@gmail.com>
2017-06-14 16:09:12 +0300

    minor changes

--- myweb/settings.py
+++ myweb/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os.path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,7 +119,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'STATIC')
 
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 import dj_database_url
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #ALLOWED_HOSTS = 'electronic-app.herokuapp.com'
